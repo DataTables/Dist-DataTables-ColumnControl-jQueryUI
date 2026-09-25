03f4efc5d995fb5b65bcaf72fc5f4c36234bc4bc
dtbuild <dtbuild@rigil>
2026-09-25 11:14:05 +0000

    9c1b5dd693ea913fdfad3f8d98255e6565609e86 Release 2.1.2
    Sync to source repo @9c1b5dd693ea913fdfad3f8d98255e6565609e86

--- js/columnControl.jqueryui.min.mjs
+++ js/columnControl.jqueryui.min.mjs
@@ -1,4 +1,4 @@
-/*! ColumnControl jQuery UI styling 2.1.1 for DataTables
+/*! ColumnControl jQuery UI styling 2.1.2 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-jqui";import ColumnControl from"datatables.net-columncontrol";export default DataTable;
